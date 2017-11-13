129089fac12a57e6ffdccd03094476cb3a3084b1
funkeydiscoman <jacksonrfrank@gmail.com>
2017-11-13 10:40:32 -0800

    setting it up

---
+++ src/ElementRemover.java
@@ -0,0 +1,22 @@
+/*
+ Jackson Frank
+ File: ElementRemover.java
+ Purpose: To remove a range of elements from a list using using certain parameters
+ */
+
+import java.util.*;
+
+public class ElementRemover {
+
+	public static void main(String[] args) {
+		
+		LinkedList<String> list = new LinkedList<String>();
+		list.add("hello");
+		list.add("hi");
+		list.add("bonjour");
+		list.add()
+		
+
+	}
+
+}
